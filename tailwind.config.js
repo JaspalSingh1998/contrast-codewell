1de3d8ad911fbc323192812fbc6a33967a7a92f6
Jaspal Singh <jaspalsinghpersonal9@gmail.com>
2021-08-21 13:26:18 +0530

    Project Setup and Navigation Bar

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: ["./src/**/*.html", "./src/**/*.js"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      Archivo: ["Archivo", "sans-serif"],
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
