22e06825334e12b3b749cf3680e7400cfff753b7
Jaspal Singh <jaspalsinghpersonal9@gmail.com>
2021-08-21 14:06:12 +0530

    Fixed Spacing Issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,15 @@ module.exports = {
     fontFamily: {
       Archivo: ["Archivo", "sans-serif"],
     },
+    minHeight: {
+      0: "0",
+      "1/4": "25%",
+      "1/2": "50%",
+      "3/4": "75%",
+      full: "100%",
+      custom: "80vh",
+      screen: "100vh",
+    },
     extend: {
       colors: {
         body: "#595959",
